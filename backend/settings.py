03a4393cf308c4d575ca3876f173d3e72239a3eb
mbugua <email@email.com>
2021-03-17 20:24:38 +0300

    procFinal

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-import heroku as heroku
+# import heroku as heroku
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
